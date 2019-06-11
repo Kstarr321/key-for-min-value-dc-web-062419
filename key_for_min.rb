93caba195e9c9e0ae606a2a96e0c81cac0c9ef13
Kolton Starr <starrkolton@gmail.com>
2019-06-11 20:55:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,20 +2,3 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 
-def key_for_min_value(hash)
-i = 0
-min_val = hash[:apple]
-min_tin = "you"
-  hash.each do |item, value|
-
-  if value < min_val 
-    min_val = value 
-    min_tin = item 
-      
-
-
-    end 
-  end 
-  min_val
-  min_tin
-end
